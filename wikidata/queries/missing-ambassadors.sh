d99276935898013e7604d614d8a95f48b7b62b19
Tony Bowden <tony@tmtm.com>
2023-01-15 08:33:35 +0200

    make missing-ambassadors more robust

--- wikidata/queries/missing-ambassadors.sh
+++ wikidata/queries/missing-ambassadors.sh
@@ -3,4 +3,4 @@
 RESULTS=$(mktemp)
 
 wd sparql wikidata/templates/ambassadors.js -f csv | sed -e 's^http://www.wikidata.org/entity/^^g' > $RESULTS
-qsv join --left-anti item $RESULTS position <(qsv cat rows wikidata/*.csv) > wikidata/results/missing-ambassadors.csv
+qsv join --left-anti item $RESULTS position <(qsv cat rows wikidata/*positions.csv) > wikidata/results/missing-ambassadors.csv
